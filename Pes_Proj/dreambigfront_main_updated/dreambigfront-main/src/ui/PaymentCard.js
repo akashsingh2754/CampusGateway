f48c697541820bf2370d19f4c24ad1f619aa4b28
Akash Singh <akashxd40@gmail.com>
2023-05-31 18:03:03 +0530

    CampusGateway

--- Pes_Proj/dreambigfront_main_updated/dreambigfront-main/src/ui/PaymentCard.js
+++
@@ -1,9 +0,0 @@
-import classes from './PaymentCard.module.css';
-
-function PaymentCard(props){
-return(    
-<div className={classes.card}style={props.style}>{props.children}</div>
-);
-}
-
-export default PaymentCard;
